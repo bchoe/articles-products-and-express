e8f2ebb440ec78a6d9336b9464f477b97f226afb
Bryan Choe <bryan.choe88@gmail.com>
2016-10-10 16:17:01 -1000

    enabled post

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
+let newProd = [];
+let productId = 0;
 
 router.route('/')
   .get((req,res) => {
-
+    res.render('index', {
+      title: 'Products'
+    });
   })
 
   .post((req,res) => {
-
+    let product = {
+      id: productId,
+      name: req.body.name,
+      price: req.body.price,
+      inventory: req.body.inventory
+    };
+    newProd.push(product);
+    productId += 1;
+    console.log(newProd);
+    res.send(`{"success": true}`);
   });
+
 
 router.route('/:id')
   .put((req,res) => {
@@ -24,6 +38,12 @@ router.route('/:id/edit')
 
   });
 
+router.route('./new')
+  .get((req,res) => {
+    res.render('index', {
+
+    });
+  });
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,20 +4,22 @@ const app = express();
 const PORT = 3000;
 const articles = require('./routes/articles.js');
 const products = require('./routes/products.js');
+const bodyParser = require('body-parser');
 
 //set template engine to use pug
 app.set('view engine', 'pug');
 //tell express where our template files live
 app.set('views', './templates');
-//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('./public'));
+
 app.use('/articles', articles);
 app.use('/products', products);
 
 app.get('/', (req,res) => {
   //use res.render to render static HTML file
   res.render('index', {
-    response: 'Pen pineapple, apple pen'
+    response: 'Hey there'
   });
 });
 
